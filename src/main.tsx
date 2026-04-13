1ac3f891a7472517aeb40034b446e25b5d0a724d
charleymoraes-commits <charley.moraes@gmail.com>
2026-04-13 23:50:16 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,5 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>
 <App/>
-</React.StrictMode>,)                                                          
-                                                             
-                                                      
+</React.StrictMode>,) 
+<script type="module" src="/src/main.tsx"></script>
