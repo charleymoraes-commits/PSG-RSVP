94f531bcad72bfe07a403d53101a875d30572510
charleymoraes-commits <charley.moraes@gmail.com>
2026-04-15 20:34:53 +0800

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
